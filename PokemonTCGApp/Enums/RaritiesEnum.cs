119b87ab225dbf4d4eaecbb7ff77e2cd69e17034
Yen Lagoon <fire337972@gmail.com>
2022-04-11 01:33:06 +0800

    Enum建立

--- PokemonTCGApp/Enums/RaritiesEnum.cs
+++ PokemonTCGApp/Enums/RaritiesEnum.cs
@@ -2,28 +2,53 @@
 {
     public enum RaritiesEnum
     {
-        //"Amazing Rare",
-        //"Common",
-        //"LEGEND",
-        //"Promo",
-        //"Rare",
-        //"Rare ACE",
-        //"Rare BREAK",
-        //"Rare Holo",
-        //"Rare Holo EX",
-        //"Rare Holo GX",
-        //"Rare Holo LV.X",
-        //"Rare Holo Star",
-        //"Rare Holo V",
-        //"Rare Holo VMAX",
-        //"Rare Prime",
-        //"Rare Prism Star",
-        //"Rare Rainbow",
-        //"Rare Secret",
-        //"Rare Shining",
-        //"Rare Shiny",
-        //"Rare Shiny GX",
-        //"Rare Ultra",
-        //"Uncommon"
+        
+        [Description("常見")]  // 寶可夢、物品卡、能量卡
+        C = 1,
+
+        [Description("不常見")]  //寶可夢、支援者卡、物品卡、競技場卡、能量卡
+        U = 2,
+
+        [Description("稀有")]  //寶可夢、支援者卡、物品卡、競技場卡
+        R = 3,
+
+        [Description("雙重稀有")]  //寶可夢EX、寶可夢GX、寶可夢V
+        RR = 4,
+
+        [Description("三重稀有")]  //寶可夢VMAX、寶可夢V-UNION、寶可夢VSTAR
+        RR = 5,
+
+        [Description("非常稀有")]  //寶可夢GX、寶可夢V、支援者卡
+        SR = 6,
+
+        [Description("超級稀有")]  //寶可夢GX、寶可夢VMAX、支援者卡
+        HR = 7,
+
+        [Description("極其稀有")]  //寶可夢、寶可夢GX、寶可夢V、寶可夢VMAX、物品卡、競技場卡、能量卡
+        UR = 8,
+
+        [Description("稜柱之星稀有")]  //稜柱之星
+        PR = 9, 
+
+        [Description("訓練家稀有")]  //支援者卡、物品卡
+        TR = 10, 
+
+        [Description("奇幻稀有")]  //寶可夢
+        AR = 11,
+
+        [Description("光輝")]  //寶可夢
+        K = 12,
+
+        [Description("角色稀有")]  //寶可夢
+        CHR = 13,
+
+        [Description("角色非常稀有")]  //寶可夢V、寶可夢VMAX
+        CSR = 14,
+
+        [Description("異色")]  //寶可夢
+        S = 15,
+
+        [Description("異色非常稀有")]  //寶可夢GX、寶可夢V、寶可夢VMAX
+        SSR = 16,
     }
 }
--- PokemonTCGApp/Enums/SubtypesEnum.cs
+++ PokemonTCGApp/Enums/SubtypesEnum.cs
@@ -4,29 +4,69 @@ namespace PokemonTCGApp.Enums
 {
     public enum SubtypesEnum
     {
-        //BREAK,
-        //Baby,
+        ////BREAK,
+        ////Baby,
+        //[Description("基礎")]
+        //Basic = 1,
+        ////EX,
+        //[Description("GX")]
+        //GX = 2,
+        ////Goldenrod Game Corner,
+        //[Description("物品")]
+        //Item = 3,
+        ////LEGEND,
+        ////Level-Up,
+        ////MEGA,
+        ////Pokémon Tool,
+        ////Pokémon Tool F,
+        //[Description("連擊")]
+        //RapidStrike = 4,
+        ////Restored,
+        ////Rocket's Secret Machine,
+        //[Description("一擊")]
+        //SingleStrike = 5,
+        //[Description("特殊能量")]
+        //Special = 6,
+        //[Description("競技場")]
+        //Stadium = 7,
+        //[Description("進化一")]
+        //Stage1,
+        //[Description("進化二")]
+        //Stage2,
+        //[Description("支援者")]
+        //Supporter,
+        //[Description("TAGTEAM")]
+        //TAGTEAM,
+        ////Technical Machine,
+        //[Description("V")]
+        //V,
+        //[Description("VMAX")]
+        //VMAX,
+        //[Description("VUnion")]
+        //VUnion,
+        //[Description("VSTAR")]
+        //VSTAR,
+        //[Description("光輝寶可夢")]
+        //Sparkling
+
+        
         [Description("基礎")]
         Basic = 1,
-        //EX,
         [Description("GX")]
-        GX = 2,
         //Goldenrod Game Corner,
         [Description("物品")]
         Item = 3,
-        //LEGEND,
-        //Level-Up,
-        //MEGA,
-        //Pokémon Tool,
-        //Pokémon Tool F,
+        
         [Description("連擊")]
         RapidStrike = 4,
         //Restored,
         //Rocket's Secret Machine,
         [Description("一擊")]
         SingleStrike = 5,
-        Special,
-        Stadium,
+        [Description("特殊能量")]
+        Special = 6,
+        [Description("競技場")]
+        Stadium = 7,
         [Description("進化一")]
         Stage1,
         [Description("進化二")]
