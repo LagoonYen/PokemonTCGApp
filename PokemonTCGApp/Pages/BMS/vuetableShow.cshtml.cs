c9442d575db9761b198ecb5d6ec1d5c9c2b77835
Yen Lagoon <fire337972@gmail.com>
2022-04-12 15:41:18 +0800

    cardlist 頁面建置3成 正常讀取 但功能簡陋
    vuetableShow example頁面
    功能強大
    但不會用 贛
    
    開始建立新增卡片頁面

---
+++ PokemonTCGApp/Pages/BMS/vuetableShow.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PokemonTCGApp.Pages.BMS
+{
+    public class vuetableShowModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
