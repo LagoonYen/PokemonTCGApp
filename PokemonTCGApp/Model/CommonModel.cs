33eeb61b0ea2d1667e671edee16259c89dd7db7e
Yen Lagoon <fire337972@gmail.com>
2022-04-15 13:57:26 +0800

    el-upload 設置完善 1. 圖片上傳後 input消失 2. 圖片移除後 input顯示 3. 縮圖框功能 4. 等待API串接

---
+++ PokemonTCGApp/Model/CommonModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PokemonTCGApp.Model
+{
+    public class ImageInfo
+    {
+        public string? ImageId { get; set; }
+        public string? ImageUrl { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        //public int SortIndex { get; set; }
+        //public string NavigationUrl { get; set; }//( 應該沒有)
+    }
+}
